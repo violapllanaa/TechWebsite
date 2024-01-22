b2954ce43db48ff446856ed5fe150f6d808ba02d
violapllana <147282717+violapllana@users.noreply.github.com>
2024-01-22 14:22:50 +0100

    commit

---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tech Website</title>
+    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" type="text/css" 
+    href="https://cdnjs.cloudflare.com
+    /ajax/libs/font-awesome/5.15.1/css/all.min.css">
+</head>
+<body>
+    <div class="contanier">
+    <div class="header">
+   <div class="menu">
+     <a class="menu-link" href="./index.html">Home</a>
+     <a class="menu-link" href="AboutUs.html">About</a>
+     <a class="menu-link" href="Contact.html">Contact</a>
+      <a class="menu-link" href="Products.html">Products</a>
+     </div>
+      <div class="title">
+      <h1><span class="tech">Tech</span> Website</h1>
+     </div>
+      <div class="search-box">
+
+     <input type="text" class="text" placeholder="search"> </p>
+      <button class="logIn_btn"><a href="./LogIn.html" alt="login">Log in</a></button>
+        
+    </div>
+          <div class="icons"></div>
+        </div>
+        <div class="main">
+            <div class="paragraph_main">
+                <h1>The future is now..</h1>
+                <h3>Check our latest products</h3>
+                <a href="products.html" class="button">Shop now</a>
+            </div>
+            <div class="paragraph_img">
+            </div>
+        </div>
+       
+        <div class="g"></div>
+        <div class="footer">
+                <div class="row">
+                    <div class="footer-col">
+                <h4>company</h4>
+              <ul>
+            <li><a href="#">about us</a></li>
+           <li><a href="#">our services</a></li>
+          <li><a href="#">privacy policy</a></li>
+        <li><a href="#">affiliate program</a></li>
+             </ul>
+            </div>
+       </footer>
+          <div class="footer-col">
+          <h4>get help</h4>
+           <ul>
+              <li><a href="#">FAQ</a></li>
+          <li><a href="#">shipping</a></li>
+          <li><a href="#">returns</a></li>
+          <li><a href="#">order status</a></li>
+          <li><a href="#">payment options</a></li>
+         </ul>
+        </div>
+            <div class="footer-col">
+            <h4>online shop</h4>
+            <ul>
+        <li><a href="#">watch</a></li>
+          <li><a href="#">bag</a></li>
+         <li><a href="#">shoes</a></li>
+          <li><a href="#">dress</a></li>
+           </ul>
+             </div>
+          <div class="footer-col">
+           <h4>follow us</h4>
+         <div class="social-links">
+            <a href="#"><i class="fab fa-facebook-f"></i></a>
+            <a href="#"><i class="fab fa-twitter"></i></a>
+            <a href="#"><i class="fab fa-instagram"></i></a>
+            <a href="#"><i class="fab fa-linkedin-in"></i></a>
+         </div>
+         </div>
+        </div>
+    </div>
+</div>
+ 
+</body>
+</html>
