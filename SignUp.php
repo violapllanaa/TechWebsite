32764da874fa7396fb7e3abe39274cbb5b72961e
Ardrinaliu <ardrinzc@gmail.com>
2024-01-24 23:40:50 +0100

    Commit

--- SignUp.php
+++ SignUp.php
@@ -12,7 +12,7 @@
 <html>
 
 <head>
-<title>Medical Clinic- Sign up</title>
+<title>Sign up</title>
     <meta http-equiv="content-type" content="text/html;charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <link rel="stylesheet" type="text/css" href="css/Signup.css">
