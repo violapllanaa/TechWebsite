2799b3487a515928c4a27ee93f42be55044effd2
violapllana <147282717+violapllana@users.noreply.github.com>
2024-01-24 23:05:31 +0100

    commit

--- LogIn.php
+++ LogIn.php
@@ -1,3 +1,46 @@
+<?php
+$host="localhost";
+$user = "root";
+$password="";
+$db="user";
+
+session_start();
+
+$data=mysqli_connect($host,$user,$password,$db);
+if($data===false){
+    die("Connection error");
+}
+if($_SERVER["REQUEST_METHOD"]=="POST")
+{
+$username=$_POST["username"];
+$password=$_POST["password"];
+
+$sql="select * from login where username='".$username. "' AND  password='".$password. "' ";
+
+$result = mysqli_query($data,$sql); 
+
+$row=mysqli_fetch_array($result);
+
+ if ($row["usertype"]== "user")
+ {
+    $_SESSION["username"]=$username;
+
+   header("location:user.php");
+echo ("user");
+ }
+ else if($row["usertype"]== "admin"){
+
+    $_SESSION["username"]=$username;
+    
+    header("location:admin.php");
+    echo ("admin");
+ }
+ else {
+    echo "username or password is incorrect";
+ }
+
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,26 +48,24 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
   <link rel="stylesheet" type="text/css" href="./css/LogIn.css">
-  <link rel="icon" type="image/svg" href="./assets/logos.png" sizes="16*16">
+  
   <title>TechWebsite- LOG IN</title>
 </head>
-
 <body>
-  <form class="form">
+  <form class="form" action = "#" method = "POST ">
     <div class="container ">
       <div class="login_form">
         <h1 >LOG IN</h1>
         <div class="relative">
-          <label for="full-name" class="leading">Email Address</label>
-          <input type="email" id="email" name="email" class="email">
+          <label for="full-name" class="leading">Username</label>
+          <input type="text" id="username" name="username" class="username" required>
         </div>
         <div class="relative">
           <label for="password" class="leading">Password</label>
-          <input type="password" id="password" name="password" class="email">
+          <input type="password" id="password" name="password" class="password" required>
           <div class="btn">
-          <button class="">Login</button>
+           <input type="submit" value = "Log In" class = "button">
           </div>
           <p class="forgetPsw">Forget password! Please contact adminstration.We care about your data</p>
        
@@ -33,7 +74,7 @@
       </div>
 
     </div>
-    <p>If you don't have an account. <a href="signup.php" >Sign up </a></p>
+    <p>If you don't have an account. <a href="SignUp.php" >Sign up </a></p>
   </form>
 </body>
 </html>
---
+++ admin.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+if(!isset($_SESSION["username"])){
+
+    header("Location:LogIn.php");
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>THIS IS ADMIN PAGE</h1>
+    <?php echo   $_SESSION["username"]  ?>
+
+    <a href="LogOut.php">Logout</a>
+</body>
+</html>
