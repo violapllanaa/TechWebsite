d87753657691906d8e31f3240ce95e544b6accaf
Ardrinaliu <ardrinzc@gmail.com>
2024-01-25 21:19:06 +0100

    Commit

--- LogIn.php
+++ LogIn.php
@@ -53,28 +53,42 @@ echo ("user");
   <title>TechWebsite- LOG IN</title>
 </head>
 <body>
-  <form class="form" action = "#" method = "POST ">
-    <div class="container ">
-      <div class="login_form">
-        <h1 >LOG IN</h1>
-        <div class="relative">
-          <label for="full-name" class="leading">Username</label>
-          <input type="text" id="username" name="username" class="username" required>
-        </div>
-        <div class="relative">
-          <label for="password" class="leading">Password</label>
-          <input type="password" id="password" name="password" class="password" required>
-          <div class="btn">
-           <input type="submit" value = "Log In" class = "button">
-          </div>
-          <p class="forgetPsw">Forget password! Please contact adminstration.We care about your data</p>
-       
-        </div>
-       
-      </div>
+  <div class="header">
+        <a href="index.php"><img src="img/Logo2.png" width="70px" height="70px"></a>
 
+        <?php if(isset($_SESSION['name'])): ?>
+        <p>Welcome, <?php echo $_SESSION['name'];?>
+        <?php if($_SESSION['user_type'] == "admin" ): ?>
+            <a href="admin.php">(Admin Panel)</a>
+        <?php endif; ?>
+        / <a href="LogOut.php">Logout</a></p>
+    <?php else: ?>
+        <p><a href="LogIn.php">Login</a> / <a href="SignUp.php">Sign up</a></p>
+    <?php endif; ?>
     </div>
-    <p>If you don't have an account. <a href="SignUp.php" >Sign up </a></p>
-  </form>
+    </div>
+<div class="container">
+    <h1 style="text-align: center;">Login:</h1>
+    <p style="text-align:center;color:red;">
+      <?php require 'includes/loginerrors.inc.php'; ?>
+    </p>
+    <div class="forma">
+    <form name="loginForm" action="includes/login.inc.php" onsubmit="return validateLogin()" method="post" required>
+        <label for="email">Enter your e-mail:</label><br>
+        <input type="email" id="email" name="email" placeholder="Enter your email..." >
+        <br>
+        <label for="password">Enter your password:</label><br>
+        <input type="password" id="password" name="password" placeholder="Enter your password..."  required>
+        <br>
+        <br>
+        <input id="button" type="submit" name="submit" value="Login">
+    </form>
+    <p>If you don't have an account. <a href="signup.php">Sign up here</a></p>
+    </div>
+    </div>
+
+
+
+</div>
 </body>
 </html>
