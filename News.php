a6c8958dc878d1c205c19cfb088ca38cb206c8ed
violapllana <147282717+violapllana@users.noreply.github.com>
2024-01-29 18:00:19 +0100

    commit

--- News.php
+++ News.php
@@ -4,49 +4,45 @@
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Tech Website- News</title>
-   <link rel="stylesheet" href="./css/News.css">
+   <link rel="stylesheet" href="css/News.css">
+   
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+   <script src="js/slide.js" defer></script>
 </head>
 <body>
+  
 <?php
    include './includes/headerIndex.php';
 ?>
-    <div class="maincontainer">
-        <h1 style="text-align: center">Latest News</h1>
-        <div class="slideshow-container">
-
-
-  <div class="Slider-fade">
-  <img src="img/LAPTOP HP 250.png">
-    <div class="text"><p>IOS 17.3 is out, Adding stolen deviceprotection for you Iphone</p></div>
-  </div>
-
-  <div class="Slider-fade">
-  <img src="img/Aicyber.jpeg">
-    <div class="text"><p>basdasd</p></div>
-  </div>
-
-  <div class="Slider-fade">
-
-    <img src="img/Aicyber.jpeg">
-      <div class="text"><p>AI rise will lead to increase in cyberattacks, GCHQ warns </p></div>
-  </div>
-
-  <a class="prev" onclick="nextSlide(-1)">&#10094;</a>
-  <a class="next" onclick="nextSlide(1)">&#10095;</a>
-</div>
-<br>
-
-<div style="text-align:center">
-  <span class="pika" onclick="cSlide(1)"></span>
-  <span class="pika" onclick="cSlide(2)"></span>
-  <span class="pika" onclick="cSlide(3)"></span>
-</div>
-
-<script type="text/javascript" src="js/main.js"></script>
-<script type="text/javascript" src="js/slide.js"></script>
-</div>
-<?php
+   <div class="main">
+    <h1>Latest News</h1>
+    <div class="slider-wrapper">
+      <button id="prevslide" class="slide-button ">&#8249;</button>
+      <div class="image-list">
+      <img src="img/Apple Imac 24 SILVER.jpg" alt="img1" class="image-items" width="350px" height="350px">
+      <img src="img/LENOVO PC AIO.png" alt="img2" class="image-item" width="350px" height="350px">
+      <img src="img/LAPTOP HP 250 G10.png" alt="img3" class="image-item" width="350px" height="350px">
+      <img src="img/Apple Iphone 14 plus Red.jpg" alt="img4" class="image-item" width="350px" height="350px">
+      <img src="img/Apple Iphone 14 plus Purple.jpg" alt="img5" class="image-item" width="350px" height="350px">
+      <img src="img/APPLE MACKBOOK AIR.jpg" alt="img6" class="image-item" width="350px" height="350px">
+      <img src="img/Apple Desktops.jpg" alt="img7" class="image-item" width="350px" height="350px">
+      <img src="img/LAPTOP GAMING LENOVO.jpg" alt="img8" class="image-item" width="350px" height="350px">
+      <img src="img/Samsung.jpg" alt="img9" class="image-item" width="350px" height="350px">
+      <img src="img/LAPTOP VICTUS.jpg" alt="img10" class="image-item" width="350px" height="350px">
+      </div>
+      <button id="nextslide" class="slide-button ">&#8250;</button>
+    </div>
+    <div class="slider-scrollbar">
+      <div class="scrollbar-track">
+        <div class="scrollbar-thumb"></div>
+      </div>
+    </div>
+   </div>
+   
+   <?php
    include './includes/footer.php';
 ?>
+
 </body>
 </html>
--- js/slide.js
+++ js/slide.js
@@ -1,26 +1,60 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+const initSlider = () => {
+  const imageList = document.querySelector(".slider-wrapper .image-list");
+  const slideButtons = document.querySelectorAll(".slider-wrapper .slide-button");
+  const sliderScrollbar = document.querySelector(".slider-scrollbar");
+  const scrollbarThumb = sliderScrollbar.querySelector(".scrollbar-thumb");
 
-function nextSlide(n) {
-  showSlides(slideIndex += n);
-    }
+  scrollbarThumb.addEventListener("mousedown", (e) => {
+    const startX = e.clientX;
+    const thumbPosition = scrollbarThumb.offsetLeft;
 
-function cSlide(n) {
-  showSlides(slideIndex = n);
-    }
+    const handleMouseMove = (e) => {
+      const deltaX = e.clientX - startX;
+      const newThumbPosition = thumbPosition + deltaX;
+     const maxThumbPosition = sliderScrollbar.getBoundingClientRect().width - scrollbarThumb.offsetWidth;
+      const boundedPosition = Math.max(0,Math.min(maxThumbPosition, newThumbPosition));
+     const scrollPosition = (boundedPosition /maxThumbPosition )* maxScrollLeft;
+      scrollbarThumb.style.left = `${boundedPosition}px`;
+      imageList.scrollLeft = scrollPosition;
+    };
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("Slider-fade");
-  var dots = document.getElementsByClassName("pika");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
+    const handleMouseUp = () => {
+
+      document.removeEventListener("mousemove", handleMouseMove);
+      document.removeEventListener("mouseup", handleMouseUp);
+    };
+
+    document.addEventListener("mousemove", handleMouseMove);
+    document.addEventListener("mouseup", handleMouseUp);
+  });
+
+  const maxScrollLeft = imageList.scrollWidth - imageList.clientWidth;
+
+  slideButtons.forEach(button => {
+    button.addEventListener("click", () => {
+      const direction = button.id === "prevslide" ? -1 : 1;
+      
+      const scrollAmount = imageList.clientWidth * direction;
+      imageList.scrollBy({ left: scrollAmount, behavior: "smooth" });
+    });
+    
+  });
+
+  const handleSlideButtons = () => {
+    slideButtons[0].style.display = imageList.scrollLeft <= 0 ? "none" : "block";
+    slideButtons[1].style.display = imageList.scrollLeft >= maxScrollLeft ? "none" : "block";
+  };
+
+  const updateScrollThumbPosition = () => {
+    const scrollPosition = imageList.scrollLeft;
+    const thumbPosition = (scrollPosition / maxScrollLeft) * (sliderScrollbar.clientWidth - scrollbarThumb.offsetWidth);
+    scrollbarThumb.style.left = `${thumbPosition}px`;
+  };
+
+  imageList.addEventListener("scroll", () => {
+    handleSlideButtons();
+    updateScrollThumbPosition();
+  });
+};
+
+window.addEventListener("load", initSlider);
